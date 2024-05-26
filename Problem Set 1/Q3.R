942f04fd5d03bdc9c87c285b64cc1b27895602d8
Ariz Kazani <kazani.ariz@gmail.com>
2024-05-26 18:21:01 -0400

    created repo for stat 1500

---
+++ Problem Set 1/Q3.R
@@ -0,0 +1,3 @@
+num_vec <- c(1, 2, 3, 4, 5)
+char_vec <- c('a', 'b', 'c', 'd', 'e')
+combined_vec = c(num_vec,  char_vec)
---
+++ Problem Set 1/Q4.R
@@ -0,0 +1,5 @@
+mat1 <- matrix(c(1, 2, 3, 4, 5, 6), nrow = 2, ncol = 3, byrow = TRUE)
+mat2 <- matrix(c(7, 8, 9, 10), nrow = 3, ncol = 2, byrow = TRUE)
+
+mat1 %*% mat2
+
